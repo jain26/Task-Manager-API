c2014cb3f108a99d0b7471c620315b4c0f947725
Ayush Jain <ayush.j26@gmail.com>
2020-08-29 16:01:56 +0530

    Heroku Port error solved

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const userRouter=require('../src/routers/user')
 const taskRouter=require('../src/routers/task')
 
 const app = express()
-const port = process.env.Port
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
